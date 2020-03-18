94ec3c9c0f7b4f6a926e47bed82a4c2ba11fa929
sivajisanapala <sivajis11@gmail.com>
2020-03-18 14:46:57 +0530

    ajax post and get updated

--- src/app/items/dashboard/brief/ajax/ajax.component.ts
+++ src/app/items/dashboard/brief/ajax/ajax.component.ts
@@ -11,12 +11,41 @@ export class AjaxComponent implements OnInit {
 
   ngOnInit() {
   }
- jqueryajaxcode = `
+ jqueryajaxget = `
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
-    }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
+        type:'GET',
+        url: "http://rest-service.guides.spring.io/greeting",
+        success: function(order){
+          $.each(order, function(i, order){
+            $("#our_selector").append('<span> You have ordered :'+ order.name +'for RS:'+ order.drink + '</span>');
+          });
+        },
+        error: function(){
+          alert("Error Loading Orders");
+        }
     });
+`
+
+ jqueryajaxpost = `
+
+  
+  $(".add-order").on('click', function(){
+    var NewOrder = {[}
+      name: $("#name").val(),
+      drink: $("#drink").val()
+    };
+    
+    $.ajax({
+        type:'POST',
+        url: "http://rest-service.guides.spring.io/greeting",
+        data: NewOrder;
+        success: function(NewOrder){
+          $("#our_selector").append('<span> You have ordered :'+ order.name +'for RS:'+ order.price);s
+        },
+        error: function(){
+          alert("Error posting Order")
+        };
+    });
+  });  
 `
 }
