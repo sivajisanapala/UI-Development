7213ae559413752ca176cdab7873b6f1e41ea8f0
sivajisanapala <sivajis11@gmail.com>
2020-03-20 16:03:29 +0530

    1 file modified

--- src/app/items/dashboard/brief/jscript/jscript.component.ts
+++ src/app/items/dashboard/brief/jscript/jscript.component.ts
@@ -20,4 +20,48 @@ export class JscriptComponent implements OnInit {
       uniqueArray1 = [...new Set(arr)];
       document.querySelector("#farray").innerHTML = uniqueArray1;`
 
+  arrayobjsort = `
+    $(document).ready(function() {
+    	
+		var MyData = [
+				{ id : 1, name: "Angel Miguel",   city: "Nex Mexico" },
+				{ id : 2, name: "Michael Rogers", city: "Bogotá"     },
+				{ id : 3, name: "Steve Rogers",   city: "New York"   },
+				{ id : 4, name: "Ángel José",     city: "Bucaramanga"},
+				{ id : 5, name: "Carlos Delgado", city: "Nex Mexico" },
+				{ id : 6, name: "Jhonny Zapata",  city: "Zacatecas"  },
+				{ id : 7, name: "Bruce Wayne",    city: "Gotham"     },
+				{ id : 8, name: "Speedy Gonzales",city: "Nex Mexico" }
+			];
+			/*** Function to sort alphabetically an array of objects by some specific key.
+			 * 
+			 * @param {String} property Key of the object to sort.
+			 */
+			function dynamicSort(property) {
+				var sortOrder = 1;
+
+				if(property[0] === "-") {
+					sortOrder = -1;
+					property = property.substr(1);
+					alert(property[0]);
+				}
+
+				return function (a,b) {
+					if(sortOrder == -1){
+						return b[property].localeCompare(a[property]);
+					}else{
+						return a[property].localeCompare(b[property]);
+					}        
+				}
+			}
+
+			// Sort the MyData array with the custom function
+			// that sorts alphabetically by the name key
+			MyData.sort(dynamicSort("city"));
+
+
+			// Display data with new order !
+			console.log(MyData);
+		});
+  `
 }
